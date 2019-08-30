b78ea81ff228bf6c540f636e20d7a522c4ceed3d
ivanpauno <ivanpauno@ekumenlabs.com>
2019-08-30 12:04:39 -0300

    Address per review comments
    Signed-off-by: ivanpauno <ivanpauno@ekumenlabs.com>

--- rcl/include/rcl/graph.h
+++ rcl/include/rcl/graph.h
@@ -75,7 +75,7 @@ typedef rmw_names_and_types_t rcl_names_and_types_t;
  * \return `RCL_RET_OK` if the query was successful, or
  * \return `RCL_RET_NODE_INVALID` if the node is invalid, or
  * \return `RCL_RET_INVALID_ARGUMENT` if any arguments are invalid, or
- * \return `RCL_RET_NON_EXISTENT_NODE_NAME` if the node name wasn't found, or
+ * \return `RCL_RET_NODE_NAME_NON_EXISTENT` if the node name wasn't found, or
  * \return `RCL_RET_ERROR` if an unspecified error occurs.
  */
 RCL_PUBLIC
@@ -123,7 +123,7 @@ rcl_get_publisher_names_and_types_by_node(
  * \return `RCL_RET_OK` if the query was successful, or
  * \return `RCL_RET_NODE_INVALID` if the node is invalid, or
  * \return `RCL_RET_INVALID_ARGUMENT` if any arguments are invalid, or
- * \return `RCL_RET_NON_EXISTENT_NODE_NAME` if the node name wasn't found, or
+ * \return `RCL_RET_NODE_NAME_NON_EXISTENT` if the node name wasn't found, or
  * \return `RCL_RET_ERROR` if an unspecified error occurs.
  */
 RCL_PUBLIC
@@ -170,7 +170,7 @@ rcl_get_subscriber_names_and_types_by_node(
  * \return `RCL_RET_OK` if the query was successful, or
  * \return `RCL_RET_NODE_INVALID` if the node is invalid, or
  * \return `RCL_RET_INVALID_ARGUMENT` if any arguments are invalid, or
- * \return `RCL_RET_NON_EXISTENT_NODE_NAME` if the node name wasn't found, or
+ * \return `RCL_RET_NODE_NAME_NON_EXISTENT` if the node name wasn't found, or
  * \return `RCL_RET_ERROR` if an unspecified error occurs.
  */
 RCL_PUBLIC
@@ -216,7 +216,7 @@ rcl_get_service_names_and_types_by_node(
  * \return `RCL_RET_OK` if the query was successful, or
  * \return `RCL_RET_NODE_INVALID` if the node is invalid, or
  * \return `RCL_RET_INVALID_ARGUMENT` if any arguments are invalid, or
- * \return `RCL_RET_NON_EXISTENT_NODE_NAME` if the node name wasn't found, or
+ * \return `RCL_RET_NODE_NAME_NON_EXISTENT` if the node name wasn't found, or
  * \return `RCL_RET_ERROR` if an unspecified error occurs.
  */
 RCL_PUBLIC
